c65f1eb485f59d6839490f48145e3811c11d1eed
djso89 <danielso89@gmail.com>
2020-08-12 20:46:07 -0700

    new public instance method: delete

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,10 +7,19 @@ class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
+    __filtered = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        else:
+            if type(cls) == str:
+                cls = eval(cls)
+            for key, value in self.__objects.items():
+                if type(value) == cls:
+                    self.__filtered[key] = value
+            return self.__filtered
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +57,7 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """to delete obj from __objects if it’s inside """
+        del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
