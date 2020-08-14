7180be6207628b3bfc0b28bb77f7ad5f3e892882
michedomingo <michedomingo@gmail.com>
2020-08-13 19:23:47 -0700

    Update test

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,29 +1,76 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+"""
+Unittest for class Review
+"""
+import unittest
 from models.review import Review
+from models.base_model import BaseModel
+import os
+import pep8
 
 
-class test_review(test_basemodel):
-    """ """
+class ReviewTest(unittest.TestCase):
+    """Defines tests for class Review"""
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "Review"
-        self.value = Review
+    @classmethod
+    def setUp(cls):
+        """Set up testing environment"""
+        cls.rev = Review()
+        cls.rev.place_id = "modern_honolulu321"
+        cls.rev.user_id = "modern123"
+        cls.rev.text = "Cool hotel"
 
-    def test_place_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.place_id), str)
+    @classmethod
+    def teardown(cls):
+        """Teardown test"""
+        del cls.rev
 
-    def test_user_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.user_id), str)
+    def tearDown(self):
+        """Reset testing environment"""
+        try:
+            os.remove("file.json")
+        except:
+            pass
 
-    def test_text(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.text), str)
+    def test_pep8(self):
+        """Test pep8 compliance"""
+        style = pep8.StyleGuide(quit=True)
+        result = style.check_files(['models/review.py'])
+        self.assertEqual(result.total_errors, 0, "not pep8 compliant")
+
+    def test_docstring(self):
+        """Test compliance with doctring requirements"""
+        self.assertIsNotNone(Review.__doc__)
+
+    def test_attributes(self):
+        """Test attributes"""
+        self.assertTrue('id' in self.rev.__dict__)
+        self.assertTrue('created_at' in self.rev.__dict__)
+        self.assertTrue('updated_at' in self.rev.__dict__)
+        self.assertTrue('place_id' in self.rev.__dict__)
+        self.assertTrue('text' in self.rev.__dict__)
+        self.assertTrue('user_id' in self.rev.__dict__)
+
+    def test_subclass(self):
+        """Test if instance of BaseModel successfully made"""
+        self.assertTrue(issubclass(self.rev.__class__, BaseModel), True)
+
+    def test_attribute_types(self):
+        """Test attribute types"""
+        self.assertEqual(type(self.rev.text), str)
+        self.assertEqual(type(self.rev.place_id), str)
+        self.assertEqual(type(self.rev.user_id), str)
+
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db', 'db')
+    def test_save(self):
+        """Test save"""
+        self.rev.save()
+        self.assertNotEqual(self.rev.created_at, self.rev.updated_at)
+
+    def test_to_dict(self):
+        """Test convert to dictionary"""
+        self.assertEqual('to_dict' in dir(self.rev), True)
+
+
+if __name__ == "__main__":
+    unittest.main()
