45c77429766dbb408b47d87e502d8226dbae74f0
djso89 <danielso89@gmail.com>
2020-08-14 14:51:21 -0700

    update basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,11 @@ This module defines class BaseModel  for all models in our hbnb clone
 import uuid
 from datetime import datetime
 import models
+from sqlalchemy import Integer, String , Column, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
 
 
 class BaseModel:
@@ -27,8 +32,8 @@ class BaseModel:
         try:
             self.created_at
         except Exception:
-            self.updated_at = datetime.now()
-            self.created_at = datetime.now()
+            self.updated_at = datetime.utcnow()
+            self.created_at = datetime.utcnow()
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -37,7 +42,7 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
         models.storage.new(self)
         models.storage.save()
 
@@ -45,9 +50,15 @@ class BaseModel:
         """Convert instance into dict format"""
         base_dict = {}
         base_dict["__class__"] = self.__class__.__name__
+        if "_sa_instance_state" in base_dict:
+            del base_dict["_sa_instance_state"]
         if self.__dict__:
             for key, value in self.__dict__.items():
                 if isinstance(value, datetime):
                     value = value.isoformat()
                 base_dict[key] = value
         return base_dict
+
+    def delete(self):
+        """delete the current instance from the storage (models.storage) """
+        models.storage.delete(self)
