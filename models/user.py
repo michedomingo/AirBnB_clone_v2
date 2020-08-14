bb2adb77c7f01327b51b4a474ee9080b17094e60
michedomingo <michedomingo@gmail.com>
2020-08-13 19:26:15 -0700

    Task7 solution

--- models/user.py
+++ models/user.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """
-This module defines class User that inherits from BaseModel
+This module defines class User that
+inherits from BaseModel and Base
 """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel):
@@ -13,7 +16,12 @@ class User(BaseModel):
         first_name: (str) user's first name
         last_name: (str) user's last name
     """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship('Place', backref='user',
+                          cascade='all, delete-orphan')
+    reviews = relationship('Review', backref='user',
+                           cascade='all, delete-orphan')
