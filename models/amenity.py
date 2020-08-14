175ecf313ce47b006b39799eef7e3cb91c43b0cc
cmillecan <cmillecan@gmail.com>
2020-08-14 21:42:40 +0000

    updated amenity

--- models/amenity.py
+++ models/amenity.py
@@ -2,7 +2,10 @@
 """
 This module defines class Amenity that inherits from BaseModel
 """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.place import place_amenity
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel):
@@ -10,4 +13,6 @@ class Amenity(BaseModel):
     Initialize class Amenity with attribute
         name: (str) name of the amenity
     """
-    name = ""
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place(amenity))
