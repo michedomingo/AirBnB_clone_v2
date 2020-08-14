ef81b5bca890de0cde9751d32bef9049266e10ff
michedomingo <michedomingo@gmail.com>
2020-08-13 19:24:25 -0700

    Update test

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,69 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+"""
+Unittest for class Amenity
+"""
+import unittest
 from models.amenity import Amenity
+from models.base_model import BaseModel
+import os
+import pep8
 
 
-class test_Amenity(test_basemodel):
-    """ """
+class AmenityTest(unittest.TestCase):
+    """Defines tests for class Amenity"""
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "Amenity"
-        self.value = Amenity
+    @classmethod
+    def setUp(cls):
+        """Set up testing environment"""
+        cls.amenity = Amenity()
+        cls.amenity.name = "Pool"
 
-    def test_name2(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+    @classmethod
+    def teardown(cls):
+        """Teardown test"""
+        del cls.amenity
+
+    def tearDown(self):
+        """Reset testing environment"""
+        try:
+            os.remove("file.json")
+        except:
+            pass
+
+    def test_pep8(self):
+        """Test pep8 compliance"""
+        style = pep8.StyleGuide(quit=True)
+        result = style.check_files(['models/amenity.py'])
+        self.assertEqual(result.total_errors, 0, "not pep8 compliant")
+
+    def test_docstring(self):
+        """Test compliance with doctring requirements"""
+        self.assertIsNotNone(Amenity.__doc__)
+
+    def test_attributes(self):
+        """Test attributes"""
+        self.assertTrue('id' in self.amenity.__dict__)
+        self.assertTrue('created_at' in self.amenity.__dict__)
+        self.assertTrue('updated_at' in self.amenity.__dict__)
+        self.assertTrue('name' in self.amenity.__dict__)
+
+    def test_subclass(self):
+        """Test if instance of BaseModel successfully made"""
+        self.assertTrue(issubclass(self.amenity.__class__, BaseModel), True)
+
+    def test_attribute_types(self):
+        """Test attribute types"""
+        self.assertEqual(type(self.amenity.name), str)
+
+    def test_save(self):
+        """Test save"""
+        self.amenity.save()
+        self.assertNotEqual(self.amenity.created_at, self.amenity.updated_at)
+
+    def test_to_dict(self):
+        """Test convert to dictionary"""
+        self.assertEqual('to_dict' in dir(self.amenity), True)
+
+
+if __name__ == "__main__":
+    unittest.main()
