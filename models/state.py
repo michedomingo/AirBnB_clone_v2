ec9cd5baa9399effe5f0c473f37c57b311d3990f
djso89 <danielso89@gmail.com>
2020-08-15 15:25:10 -0700

    task6

--- models/state.py
+++ models/state.py
@@ -4,6 +4,8 @@ This module defines class State that inherits from BaseModel
 """
 from models.base_model import BaseModel, Base
 import models
+import sqlalchemy import Column, String
+import sqlalchemy.orm import relationship
 from os import getenv
 
 
@@ -12,4 +14,8 @@ class State(BaseModel):
     Initialize class State with attribute
         name: (str) name of the state
     """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable="False")
+
+    @property
+    def cities(self):
